fb60dde0aba91cac0bdcd6c41432a6fce880975e
thenatural86 <philip.smalls86@gmail.com>
2024-08-27 10:15:28 -0300

    linting

--- app/app/urls.py
+++ app/app/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from drf_spectacular.views import ( # type: ignore
+from drf_spectacular.views import (  # type: ignore
     SpectacularAPIView,
     SpectacularSwaggerView,
 )
@@ -21,9 +21,10 @@ from django.contrib import admin
 from django.urls import path
 
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
-    path('api/docs/', SpectacularSwaggerView.as_view(url_name='api-schema'), name='api-docs')
+    path('api/docs/',
+         SpectacularSwaggerView.as_view(url_name='api-schema'),
+         name='api-docs')
 ]
