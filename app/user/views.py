a2851371a740ed41c1464565ac17fcc089c7f4e6
thenatural86 <philip.smalls86@gmail.com>
2024-08-28 11:00:20 -0300

    adds views and urls for updating a user

--- app/user/views.py
+++ app/user/views.py
@@ -1,7 +1,11 @@
 """
 Views for the user API.
 """
-from rest_framework import generics  # type: ignore
+from rest_framework import (  # type: ignore
+    generics,
+    authentication,
+    permissions
+)
 from rest_framework.authtoken.views import ObtainAuthToken  # type: ignore
 from rest_framework.settings import api_settings  # type: ignore
 from user.serializers import (
@@ -19,3 +23,14 @@ class CreateTokenView(ObtainAuthToken):
     """Create a new auth token for user."""
     serializer_class = AuthTokenSerializer
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """Manage the authenticated user."""
+    serializer_class = UserSerializer
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        """Retrieve and return the authenticated user."""
+        return self.request.user
