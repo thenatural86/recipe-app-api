1520df854766b805f0ea574d320bfc1e89cd935f
thenatural86 <philip.smalls86@gmail.com>
2024-08-28 10:33:34 -0300

    wip/test for manage user api

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -1,6 +1,7 @@
 """
 Tests for the user API.
 """
+from re import M
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 from django.urls import reverse
@@ -10,6 +11,7 @@ from rest_framework import status  # type: ignore
 
 CREATE_USER_URL = reverse('user:create')
 TOKEN_URL = reverse('user:token')
+ME_URL = reverse('user:me')
 
 
 def create_user(**params):
@@ -111,3 +113,38 @@ class PublicUserApiTests(TestCase):
 
         self.assertNotIn('token', res.data)  # type: ignore
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_retrieve_user_authorization(self):
+        """Test authentication is required for users."""
+        res = self.client.get(ME_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PrivateUserApiTests(TestCase):
+    """Test API requests that require authentication."""
+
+    def setUp(self):
+        self.user = create_user(
+            email='test@example.com',
+            password='testpass123',
+            name='Test Name'
+        )
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.user)  # type: ignore
+
+    def test_retrieve_profile_success(self):
+        """Test retrieving profile for logged in user."""
+        res = self.client.get(ME_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, {  # type: ignore
+            'name': self.user.name,  # type: ignore
+            'email':self.user.email
+        })
+
+    def test_post_me_not_allowed(self):
+        """Test POST is not allowed for the me endpoint."""
+        res = self.client.post(ME_URL, {})
+
+        self.assertEqual(res.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
