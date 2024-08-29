088b8ea120ab803cd68293208766523bd145c206
thenatural86 <philip.smalls86@gmail.com>
2024-08-29 10:35:51 -0300

    linting

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -199,5 +199,5 @@ class PrivateRecipeApiTests(TestCase):
         res = self.client.delete(url)
 
         self.assertEqual(res.status_code, status.HTTP_404_NOT_FOUND)
-        self.assertTrue(Recipe.objects.filter(id=recipe.id)
-                        .exists())  # type: ignore
+        self.assertTrue(Recipe.objects.filter(id=recipe.id)   # type: ignore
+                        .exists())
