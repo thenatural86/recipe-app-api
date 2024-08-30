0bd8d0e91e53ca78c9393a52954b4ea9f517eec5
thenatural86 <philip.smalls86@gmail.com>
2024-08-30 12:15:06 -0300

    changes name of recipe test

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -190,7 +190,7 @@ class PrivateRecipeApiTests(TestCase):
         self.assertFalse(Recipe.objects.filter(id=recipe.id)  # type: ignore
                          .exists())
 
-    def test_recipe_other_users_recipe_error(self):
+    def test_delete_other_users_recipe_error(self):
         """Test trying to delete another users recipe gives error."""
         new_user = create_user(email='user2@example.com', password='test123')
         recipe = create_recipe(user=new_user)
