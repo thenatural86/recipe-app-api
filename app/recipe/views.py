009e75e6e1b6617ed7214647368cb41d8fb14f5c
thenatural86 <philip.smalls86@gmail.com>
2024-09-04 19:47:10 -0300

    pass tests

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,7 +1,13 @@
 """
 Views for the recipe APIs
 """
-from rest_framework import viewsets, mixins  # type: ignore
+from rest_framework import (  # type: ignore
+    viewsets,
+    mixins,
+    status,
+    )
+from rest_framework.decorators import action  # type: ignore
+from rest_framework.response import Response  # type: ignore
 from rest_framework.authentication import TokenAuthentication  # type: ignore
 from rest_framework.permissions import IsAuthenticated  # type: ignore
 
@@ -24,11 +30,26 @@ class RecipeViewSet(viewsets.ModelViewSet):
         """Return the serializer class for request."""
         if self.action == 'list':
             return serializers.RecipeSerializer
+        elif self.action == 'upload_image':
+            return serializers.RecipeImageSerializer
+
         return self.serializer_class
 
     def perform_create(self, serializer):
         """Create a new recipe."""
         serializer.save(user=self.request.user)
+
+    @action(methods=['POST'], detail=True, url_path='upload-image')
+    def upload_image(self, request, pk=None):
+        """Upload an image to recipe."""
+        recipe = self.get_object()
+        serializer = self.get_serializer(recipe, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class BaseRecipeAttrViewSet(
