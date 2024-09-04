e6c3a4760a01b182a4c9f5ac023740ca30264e61
thenatural86 <philip.smalls86@gmail.com>
2024-09-04 15:17:25 -0300

    migrations

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -93,7 +93,7 @@ class ModelTests(TestCase):
     @patch('core.models.uuid.uuid4')
     def test_recipe_file_name_uuid(self, mock_uuid):
         """Test generating image path."""
-        uuid = 'test_uuid'
+        uuid = 'test-uuid'
         mock_uuid.return_value = uuid
         file_path = models.recipe_image_file_path(None, 'example.jpg')
 
